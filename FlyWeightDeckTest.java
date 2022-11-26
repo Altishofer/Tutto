f54c0ddaa478bb844c40e26ec7ef08de2df1b973
AdiHeiss <116070324+AdiHeiss@users.noreply.github.com>
2022-11-26 20:42:04 +0100

    Add files via upload

---
+++ FlyWeightDeckTest.java
@@ -0,0 +1,14 @@
+package Cards;
+
+import org.junit.jupiter.api.Test;
+
+class FlyWeightDeckTest {
+
+    // TODO: Pädi, add asserts, create all cards and count them
+
+    @Test
+    void getRandomCard() {
+        var card = new FlyWeightDeck();
+        card.getRandomCard();
+    }
+}
