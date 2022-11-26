dc0cc630dff46f4403debcb0f58b1b239db8c77b
AdiHeiss <116070324+AdiHeiss@users.noreply.github.com>
2022-11-26 20:44:14 +0100

    Add files via upload
    renamed FlyWeightDeckTest, created folder for CloverLeafTests

---
+++ untitled/test/Cards/CloverLeafTests/CloverLeafTest.java
@@ -0,0 +1,24 @@
+package Cards.CloverLeafTests;
+
+import org.junit.jupiter.api.Test;
+
+class CloverLeafTest {
+
+    //TODO: Adrian
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void rollIsTutto() {
+    }
+
+    @Test
+    void rollNotValid() {
+    }
+
+    @Test
+    void makeMove() {
+    }
+}
