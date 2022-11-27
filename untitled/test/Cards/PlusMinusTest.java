1bcad5463d6c78c562eae650769947f3660e20e2
C L <cedricivo.luechinger@uzh.ch>
2022-11-27 20:47:39 +0100

    plusminusTest added

---
+++ untitled/test/Cards/PlusMinusTest.java
@@ -0,0 +1,45 @@
+package Cards;
+
+import Utils.Roll;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlusMinusTest{
+
+    PlusMinus card = new PlusMinusTest.StubCardPlusMinus();
+
+    public static class StubCardPlusMinus extends PlusMinus{
+
+        private int stops = 0;
+        public StubCardPlusMinus() {
+            super();
+            roll = new StubRoll();
+        }
+
+        public void setStubNr(String command){((StubRoll) roll).setCommand(command);}
+
+        @Override
+        public boolean stopOrRoll() {
+            if (stops >= 1) { return true;}
+            stops++;
+            return false;
+        }
+    }
+
+    @Test
+    void makeMoveTuttoOnesOnce() {
+        ((PlusMinusTest.StubCardPlusMinus)card).setStubNr("tuttoOnes");
+        int result = card.makeMove().getFirst();
+        assertEquals(2000, result);
+    }
+
+    @Test
+    void makeMoveInvalid() {
+        ((PlusMinusTest.StubCardPlusMinus)card).setStubNr("invalid");
+        int result = card.makeMove().getFirst();
+        assertEquals(0, result);
+    }
+}
