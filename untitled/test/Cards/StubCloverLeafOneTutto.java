efeb9d01ecec62276a276a24f98efb2d371a08ae
AdiHeiss <116070324+AdiHeiss@users.noreply.github.com>
2022-11-27 17:27:36 +0100

    Add files via upload

---
+++ untitled/test/Cards/StubCloverLeafOneTutto.java
@@ -0,0 +1,15 @@
+package Cards;
+
+import Board.Board;
+import Utils.Tuple;
+
+public class StubCloverLeafOneTutto extends CloverLeaf {
+    @Override
+    public Tuple rollIsTutto(){
+        System.out.println("TUTTO!! -> you are one step closer to the WIN!");
+        Board.printDelimiter();
+        alreadyTutto = true;
+        ((StubRoll) roll).setCommand("invalid");
+        return new Tuple(makeMove().getFirst(), false);
+    }
+}
