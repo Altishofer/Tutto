37e0c7920ca4e69d86c00aa57c406ee27dca51b1
sandrin <sandrin@hunkeler.ws>
2022-11-29 20:25:19 +0100

    bugfix bonus

--- untitled/src/Cards/Bonus.java
+++ untitled/src/Cards/Bonus.java
@@ -3,6 +3,8 @@ package Cards;
 import Board.Board;
 import Utils.Tuple;
 
+import java.util.Scanner;
+
 public class Bonus extends Card {
     private final int BONUS_VALUE;
 
@@ -20,7 +22,22 @@ public class Bonus extends Card {
         int finalSum = aIntermediatePoints + aRoll.getPoints() + BONUS_VALUE;
         System.out.print("TUTTO!! -> you earned already " + finalSum + " points \n");
         Board.printDelimiter();
-        aIntermediatePoints = finalSum;
+        aIntermediatePoints += BONUS_VALUE;
         return new Tuple(finalSum, !stopOrRoll());
+    }
+
+    @Override
+    protected boolean stopOrRoll(){
+        Scanner scanner;
+        while (true){
+            aSleeper.doSleep();
+            int sum = aIntermediatePoints + aRoll.getPoints();
+            System.out.print("Do you want to roll again (R) or end the move (E) and earn the " + sum +" points? ");
+            scanner = new Scanner(System.in);
+            String playOrStop = scanner.nextLine();
+            Board.printDelimiter();
+            if (playOrStop.equalsIgnoreCase("e")){return true;}
+            if (playOrStop.equalsIgnoreCase("r")){return false;}
+        }
     }
 }
--- untitled/src/Dice/Dice.java
+++ untitled/src/Dice/Dice.java
@@ -8,7 +8,8 @@ public abstract class Dice {
     public static ArrayList<DiceValues> rollDice(Integer pNumberOfDice) {
         ArrayList<DiceValues> roll = new ArrayList<DiceValues>();
         for (int i = 0; i < pNumberOfDice; i++) {
-            roll.add(DiceValues.values()[new Random().nextInt(DiceValues.values().length)]);
+            roll.add(DiceValues.ONE);
+            //roll.add(DiceValues.values()[new Random().nextInt(DiceValues.values().length)]);
         }
         return roll;
     }
