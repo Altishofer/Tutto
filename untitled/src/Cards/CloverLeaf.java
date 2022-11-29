867d02956465cfcc82dba7fda82d6b7b2f2706d8
Chopstew <patrickwidmer@gmx.ch>
2022-11-29 10:04:58 +0100

    Added CloverLeaf.java by force

---
+++ untitled/src/Cards/CloverLeaf.java
@@ -0,0 +1,56 @@
+package Cards;
+
+import Board.Board;
+import Utils.Tuple;
+
+public class CloverLeaf extends AbstractCard {
+    public boolean alreadyTutto;
+
+    protected static CloverLeaf INSTANCE;
+
+    protected CloverLeaf() {
+        alreadyTutto = false;
+    }
+
+    public static synchronized CloverLeaf getInstance(){
+        if (INSTANCE == null){
+            INSTANCE = new CloverLeaf();
+        }
+        return INSTANCE;
+    }
+
+    @Override
+    public String toString(){
+        return "Cloverleaf-Card";
+    }
+
+    @Override
+    public Tuple rollIsTutto(){
+        System.out.println("TUTTO!! -> you are one step closer to the WIN!");
+        Board.printDelimiter();
+        alreadyTutto = true;
+        return new Tuple(makeMove().getFirst(), false);
+    }
+
+    @Override
+    public int rollNotValid(){
+        System.out.println("You had no luck, the Cloverleaf is gone");
+        Board.printDelimiter();
+        return 0;
+    }
+
+    @Override
+    public Tuple makeMove() {
+        roll.startOverRoll();
+        while (true){
+            printRoll();
+            if (!roll.isValid()){return new Tuple(rollNotValid(), false);}
+            roll.putAside();
+            if (roll.isTutto()){
+                if (alreadyTutto){return new Tuple(Integer.MAX_VALUE, false);}
+                return rollIsTutto();
+            }
+            roll.rollDices();
+        }
+    }
+}
